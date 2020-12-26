a8a8675725a2fd9163dd63d9c561fec9a1507aed
anishmoktan <anish.tmngo@gmail.com>
2020-12-26 13:05:39 -0500

    final

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const request = require("request");
-consst https = require("https");
+const https = require("https");
 
 
 const app = express();
@@ -33,21 +33,55 @@ app.post("/", function(req,res){
 
     const jsonData = JSON.stringify(data);
 
-    const url = "https://usX.api.mailchimp.com/3.0/lists/"
-    https.request(url, options, function(response){
+    const url = "https://us7.api.mailchimp.com/3.0/lists/67eaa47873";
+
+    const options= {
+
+        method: "POST",
+        auth: "ann1:727baf59bf7d456b1d6b1a74cd902fd-us7"
+    }
 
 
 
-    })
+    const request = https.request(url, options, function(response){
 
-})
+        if (response.statusCode === 200) {
+            res.sendFile(__dirname + "/success.html");
+        }
+        else {
+            res.sendFile(__dirname + "/failure.html");
+        }
 
 
 
-app.listen(3000, function(){
+
+        response.on("data", function(data){
+            console.log(JSON.parse(data));
+        })
+    });
+
+    request.write(jsonData);
+    request.end();
+
+
+});
+
+app.post("/failure", function(req,res){
+    res.redirect("/");
+
+});
+
+
+
+app.listen(process.env.PORT || 3000, function(){
     console.log("Server is running on port 3000.")
 });
 
+//API Key
+//2727baf59bf7d456b1d6b1a74cd902fd-us7
+
+//List ID
+//67eaa47873
 
 // const listId = "YOUR_LIST_ID";
 // const subscribingUser = {
