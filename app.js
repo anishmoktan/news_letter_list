a33f0c0b098e8563fce996a8942a695951bb5c37
anishmoktan <anish.tmngo@gmail.com>
2020-12-26 11:05:09 -0500

    integrating api

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const request = require("request");
-const e = require("express");
+consst https = require("https");
+
 
 const app = express();
 
@@ -13,12 +14,32 @@ app.get("/", function(req,res){
 });
 
 app.post("/", function(req,res){
-    var firstName= req.body.fname;
-    var lastName= req.body.lname;
-    var email= req.body.email;
-    console.log(firstName,lastName,email);
-
+    const firstName= req.body.fname;
+    const lastName= req.body.lname;
+    const email= req.body.email;
     
+    const data = {
+        members: [
+            {
+                email_address: email,
+                status: "subscribed",
+                merge_fields: {
+                    FNAME: firstName,
+                    LNAME: lastName
+                }
+            }
+        ]
+    };
+
+    const jsonData = JSON.stringify(data);
+
+    const url = "https://usX.api.mailchimp.com/3.0/lists/"
+    https.request(url, options, function(response){
+
+
+
+    })
+
 })
 
 
@@ -26,3 +47,30 @@ app.post("/", function(req,res){
 app.listen(3000, function(){
     console.log("Server is running on port 3000.")
 });
+
+
+// const listId = "YOUR_LIST_ID";
+// const subscribingUser = {
+//   firstName: "Prudence",
+//   lastName: "McVankab",
+//   email: "prudence.mcvankab@example.com"
+// };
+
+// async function run() {
+//   const response = await mailchimp.lists.addListMember(listId, {
+//     email_address: subscribingUser.email,
+//     status: "subscribed",
+//     merge_fields: {
+//       FNAME: subscribingUser.firstName,
+//       LNAME: subscribingUser.lastName
+//     }
+//   });
+
+//   console.log(
+//     `Successfully added contact as an audience member. The contact's id is ${
+//       response.id
+//     }.`
+//   );
+// }
+
+// run();
